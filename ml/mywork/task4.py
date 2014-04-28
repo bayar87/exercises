e906e527b8c623a066e1b5bcd69fb2fa087ba829
bayar87 <bayarb3@students.rowan.edu>
2014-04-28 05:36:03 -0400

    First Part task4

--- ml/mywork/task4.py
+++ ml/mywork/task4.py
@@ -23,3 +23,55 @@ Once you know what you are looking at perform the following tasks:
 Use optimal features you found above with different classifiers and evaulate your classifiers as to how general they are with relevant metrics.
 
 """
+######################################Classification & Feature Selection ############################################
+
+import numpy
+import numpy as np
+import scipy
+from scipy.stats import *
+
+
+
+data = numpy.genfromtxt('coupon_clickstream.csv', delimiter = ',')
+nbr_Samp=data.shape[0]-1
+nbr_Ftrs=data.shape[1]-1
+metrics=data[1:,:nbr_Ftrs]
+labels=data[1:,nbr_Ftrs]
+
+
+# function to remove duplicate columns (metrics)
+def duplicate_columns(data, minoccur=2):
+    ind = np.lexsort(data)
+    diff = np.any(data.T[ind[1:]] != data.T[ind[:-1]], axis=1)
+    edges = np.where(diff)[0] + 1
+    result = np.split(ind, edges)
+    result = [group for group in result if len(group) >= minoccur]
+    return result
+
+print "The duplicate metrics are %s" % duplicate_columns(metrics)
+
+#remove columns
+metrics_unique = np.delete(metrics, np.s_[24,28,30,40,46], 1)
+
+nbr_Ftrs_uq = metrics_unique.shape[1] 
+#Generate a correlation matrix
+dimCorr  = (nbr_Ftrs_uq, nbr_Ftrs_uq)
+#initialize the correlation matrix to zero entries
+r  = np.zeros(dimCorr)
+
+#initilize the number of correlated features (columns)
+nbr_Corr_Ftrs = 0
+
+
+
+#We consider two features are highly correlated if their correlation coefficient is bigger than 0.4
+for i in range(nbr_Ftrs_uq): # rows are the number of rows in the matrix. 
+    for j in range(i, nbr_Ftrs_uq):
+        d= scipy.stats.pearsonr(metrics_unique[:,i], metrics_unique[:,j])
+        r[i,j] = d[0]
+        r[j,i] = r[j,i]
+        if i!=j and r[i,j] > 0.4 and r[i,j] not in (0,1):
+        	nbr_Corr_Ftrs +=1
+        	print "The feautures %d and %d are highly correlated and their correlation coefficient is equal to %0.3f" % (i,j,r[i,j])
+
+print "There are only %d highly correlated features with correlation coefficient bigger than 0.4" % nbr_Corr_Ftrs
